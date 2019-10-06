7d80ada48823f00f7b3b75d88b6644d8b2d77fab
marcus quigley <marcusquigley@gmail.com>
2019-10-06 16:41:08 -0400

    some business objs

---
+++ Project1/After/Discount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestableCodeDemos.Project1.After
+{
+   
+
+    public interface IDiscount
+    {
+        decimal GetDiscount(decimal argument);
+
+    }
+    public class Discount : IDiscount
+    {
+        public decimal GetDiscount(decimal argument)
+        {
+            return argument;
+        }
+    }
+}
